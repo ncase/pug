3bb4dfccabb9670d216ad24d153c25ea56872a7d
Nicky Case <n@ncase.me>
2015-04-03 13:59:08 -0700

    parallax and onscreen framework

---
+++ index.md
@@ -0,0 +1,25 @@
+Hello my baby
+
+Hello my honey
+
+Hello my ragtime gal
+
+<iframe width="800" height="450" src="experiments/postmessagetest.html"></iframe>
+
+Send me a kiss by wire
+
+Baby my heart's on fire
+
+<iframe class="highlight" width="100%" height="100%" src="experiments/postmessagetest.html"></iframe>
+
+If you refuse me
+
+Honey you'll lose me
+
+Then you'll be left alone
+
+<iframe width="800" height="450" src="experiments/postmessagetest.html"></iframe>
+
+Oh baby, telephone
+
+And tell me I'm your own
---
+++ js/index.js
@@ -0,0 +1,29 @@
+/***
+
+On scroll, tell each playable iframe whether they're on screen,
+and if so, how far they are along the page.
+
+Parallax Parameter:
+< 0: below viewing screen
+0 < x < 1: on screen
+> 1: above viewing screen
+
+***/
+var playables = document.querySelectorAll("#essay iframe");
+window.onscroll = function(){
+	var scrollY = window.pageYOffset;
+	var innerHeight = window.innerHeight;
+	for(var i=0;i<playables.length;i++){
+		var p = playables[i];
+		var top = p.offsetTop-innerHeight;
+		var bottom = p.offsetTop+parseInt(p.height=="100%" ? innerHeight : p.height);
+		var parallax = (scrollY-top)/(bottom-top); // from 0 to 1
+		p.contentWindow.postMessage({
+			onScreen: (0<parallax && parallax<1),
+			parallax: parallax
+		},"*");
+	}
+};
+window.onload = function(){
+	window.onscroll();
+};
